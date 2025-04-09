3f49cffc256d976650071dbeb8c243de13205e19
gkirschnxr <kirschnxr@gmail.com>
2025-04-09 15:16:21 -0300

    Ajustado menu principal

--- GestaoDeEquipamentos.ConsoleApp/Compartilhado/TelaPrincipal.cs
+++ GestaoDeEquipamentos.ConsoleApp/Compartilhado/TelaPrincipal.cs
@@ -8,19 +8,17 @@ public class TelaPrincipal
 
         Console.WriteLine("----------------------------------------");
         Console.WriteLine("|        Gestão de Equipamentos        |");
-        Console.WriteLine("----------------------------------------");
+        Console.WriteLine("----------------------------------------\n");
 
-        Console.WriteLine();
-
-        Console.WriteLine("1 - Controle de Equipamentos");
-        Console.WriteLine("2 - Controle de Chamados");
-        Console.WriteLine("3 - Controle de Fabricantes");
+        Console.WriteLine("1 - Controle de Fabricantes");
+        Console.WriteLine("2 - Controle de Equipamentos");
+        Console.WriteLine("3 - Controle de Chamados");
         Console.WriteLine("S - Sair");
 
         Console.WriteLine();
 
         Console.Write("Escolha uma das opções: ");
-        char opcaoEscolhida = Console.ReadLine()[0];
+        char opcaoEscolhida = Console.ReadLine()![0];
 
         return opcaoEscolhida;
     }
